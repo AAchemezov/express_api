9b871643f15ea478c189b6da064d93a1f51afba0
a.a.chemezov <a.a.chemezov@gmail.com>
2022-05-01 20:11:56 +0800

    9.51

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 
+import {userRouter} from './users/users.js'
+
 const PORT = 8000
 const app = express()
 
-app.listen(PORT, ()=>{
-    console.log(`Сервер запущен на http://localhost:${PORT}`)
+app.get('/hello', (req, res) => {
+    res.send('Привет!')
 })
 
-app.get('/hello', (req, res)=>{
-    res.send('Привет!')
+app.use('/users', userRouter)
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`)
 })
