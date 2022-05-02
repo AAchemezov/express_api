bc34ab3c3772d23a6dae0103d3857057725424b2
a.a.chemezov <a.a.chemezov@gmail.com>
2022-05-02 17:22:28 +0800

    11.67

---
+++ src/common/base.controller.ts
@@ -0,0 +1,35 @@
+import {LoggerService} from "../logger/logger.service";
+import {Response, Router} from 'express'
+import {IControllerRoute} from "./route.interface";
+
+export abstract class BaseController {
+    private readonly _router: Router
+
+    constructor(private logger: LoggerService) {
+        this._router = Router()
+    }
+
+    get router() {
+        return this._router
+    }
+
+    public send<T>(res: Response, code: number, message: T) {
+        res.type('application/json')
+        res.status(200).json(message)
+    }
+
+    public ok<T>(res: Response,  message: T) {
+       return this.send<T>(res, 200, message)
+    }
+
+    public created(res: Response) {
+        return res.sendStatus(201)
+    }
+
+    protected bindRoutes(routes: IControllerRoute[]) {
+        routes.forEach(({func, method, path}) => {
+            this.logger.log(`[${method}] ${path}`)
+            this.router[method](path, func.bind(this))
+        })
+    }
+}
