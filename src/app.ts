05f6f5a39c9c194c08aa15dd0f768d114550cce4
a.a.chemezov <a.a.chemezov@gmail.com>
2022-05-04 11:52:06 +0800

    15.87

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { ILogger } from './logger/logger.interface'
 import { IExceptionFilter } from './errors/exception.filter.interface'
 import { IUserController } from './users/user.controller.interface'
 import { IConfigService } from './config/config.service.interface'
+import { PrismaService } from './database/prisma.service'
 
 @injectable()
 export class App {
@@ -21,6 +22,7 @@ export class App {
 		@inject(TYPES.UserController) private userController: IUserController,
 		@inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
 		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.PrismaService) private prismaService: PrismaService,
 	) {
 		this.app = express()
 		this.port = 8000
@@ -45,6 +47,7 @@ export class App {
 		this.useMiddleWare()
 		this.useRoutes()
 		this.useExceptionFilters()
+		await this.prismaService.connect()
 		this.server = this.app.listen(port)
 		this.logger.log(`Сервер запущен на http://localhost:${port}`)
 	}
