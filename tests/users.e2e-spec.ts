ea653cde95122fbb237a04b284503c25e3242613
a.a.chemezov <a.a.chemezov@gmail.com>
2022-05-06 16:25:13 +0800

    17.98

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -13,8 +13,45 @@ describe('Users e2e', () => {
 	it('Register - error', async () => {
 		const { statusCode } = await request(application.app)
 			.post('/users/register')
-			.send({ email: 'spec@email.com', password: '1' })
+			.send({ email: 'spec@email.com', password: '_' })
+
 		expect(statusCode).toBe(422)
+	})
+
+	it('Login - success', async () => {
+		const { body } = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'user@example.info', password: 'password' })
+
+		expect(body.jwt).not.toBeUndefined()
+	})
+
+	it('Login - error', async () => {
+		const { statusCode } = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'user@example.info', password: '_' })
+
+		expect(statusCode).toBe(401)
+	})
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'user@example.info', password: 'password' })
+
+		const { statusCode, body } = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`)
+
+		expect(statusCode).toBe(200)
+		expect(body.email).toBe('user@example.info')
+	})
+
+	it('Info - error', async () => {
+		const { statusCode } = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer 1`)
+		expect(statusCode).toBe(401)
 	})
 })
 
